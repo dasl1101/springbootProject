582ce92ad582cc7f44b847ea5c73d6a3dde1e245
dasl1101 <ektmf1101@gmail.com>
2023-05-26 00:27:50 +0900

    게시글 수정 삭제 기능추가

--- src/main/java/com/example/springbootProject/service/posts/PostsService.java
+++ src/main/java/com/example/springbootProject/service/posts/PostsService.java
@@ -9,8 +9,6 @@ import com.example.springbootProject.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,24 +18,25 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto)
-    {
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto){
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
         posts.update(requestDto.getTitle(), requestDto.getContent());
+
         return id;
     }
 
-    public PostsResponseDto findById (Long id){
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
-        return new PostsResponseDto(entity);
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
+        return new PostsResponseDto(entity);
     }
 
     @Transactional(readOnly = true)
@@ -47,5 +46,10 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
-
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
 }
--- src/main/java/com/example/springbootProject/web/IndexController.java
+++ src/main/java/com/example/springbootProject/web/IndexController.java
@@ -1,17 +1,18 @@
 package com.example.springbootProject.web;
 
 import com.example.springbootProject.service.posts.PostsService;
+import com.example.springbootProject.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
-
 
     @GetMapping("/")
     public String index(Model model){
@@ -20,9 +21,15 @@ public class IndexController {
     }
 
     @GetMapping("/posts/save")
-    public String postSave() {
+    public String postsSave() {
         return "posts-save";
     }
 
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
 
+        return "posts-update";
+    }
 }
